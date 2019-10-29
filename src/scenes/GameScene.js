627a565e6461241dac941d14dd7434b579041393
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-10-29 15:58:44 +0700

    [Init] initial commit

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,22 @@
+class GameScene extends Phaser.Scene{
+
+    constructor(){
+        super({
+            key: 'GameScene'
+        })
+    }
+
+    preload(){
+
+    }
+    
+    create(){
+
+    }
+
+    update(delta, time){
+         
+    }
+
+}
+export default GameScene
