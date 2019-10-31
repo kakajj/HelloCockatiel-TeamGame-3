e67733fd5ca009290c75b4f25a2542f895df7ae4
Jednuth <jednuth.tatirat@mail.kmutt.ac.th>
2019-10-31 18:23:20 +0700

    [ADD] Player

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,3 +1,4 @@
+let player
 class GameScene extends Phaser.Scene{
 
     constructor(){
@@ -7,10 +8,12 @@ class GameScene extends Phaser.Scene{
     }
 
     preload(){
+        this.load.image('Player','../../images/Gokuตัดเองจ้า.png',{frameWidth :384 ,frameHeight:216})
 
     }
     
     create(){
+         player = this.physics.add.image(384,216,'Player').setScale(0.3).setSize(0.3)
 
     }
 
