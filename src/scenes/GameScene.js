379177fd4d980bc4ab9e84ab4d9c6b3868651b4f
Jednuth <jednuth.tatirat@mail.kmutt.ac.th>
2019-10-31 18:49:19 +0700

    [UPDATE]  กำจัด Block การเดิน

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -14,16 +14,16 @@ class GameScene extends Phaser.Scene{
     }
     
     create(){
+        player = this.physics.add.image(384,216,'Player').setSize(800,600).setScale(0.2).setCollideWorldBounds(true);
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-         player = this.physics.add.image(384,216,'Player').setScale(0.3).setSize(0.3)
+        
 
     }
 
     update(){
-        bg.tilePositionX -= 1
         {
             if (keyA.isDown) {
                 player.setVelocityX(-500)
