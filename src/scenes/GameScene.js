e0fba04bbbf6cff6a0787641947fe76bf05f7e28
pattarapol <pattarapol.plasma@mail.kmutt.ac.th>
2019-10-31 18:27:19 +0700

    [ADD]inputKeyboard & player image

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,3 +1,6 @@
+let bg
+let player
+let keyW,keyA,keyS,keyD
 class GameScene extends Phaser.Scene{
 
     constructor(){
@@ -7,15 +10,33 @@ class GameScene extends Phaser.Scene{
     }
 
     preload(){
-
+            this.load.image('bg','..images/sky.png')
+            this.load.image('player','../images/Gokuตัดเองจ้า.png')
     }
     
     create(){
+        bg = this.add.tileSprite(0, 0, 384, 216, 'bg').setOrigin(0, 0).setSize(800, 600).setScale(2.8)
+        player = this.physics.add.image(384, 216, 'player').setSize(400, 300).setScale(0.2).setCollideWorldBounds(true)
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
     }
 
-    update(delta, time){
-         
+    update(){
+        bg.tilePositionX -= 1
+        {
+            if (keyA.isDown) {
+                player.setVelocityX(-500)
+            } else if (keyD.isDown) {
+                player.setVelocityX(500)
+            } else if (keyS.isDown) {
+                player.setVelocityY(+330)
+            } if (keyW.isDown) {
+                player.setVelocityY(-330)
+            }
+        }
     }
 
 }
