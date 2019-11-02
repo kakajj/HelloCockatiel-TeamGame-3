f687f6249936753cadeb924152809d2695184501
JJ <yinyin.domo@hotmail.com>
2019-11-02 16:50:33 +0700

    [UPDATE] พื้นหลัง
     จำกัดพื้นที่การเดินของผู้เล่นให้เดินได้เฉพาะบนหญ้า

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
+import { platform } from "os"
+
 var background
 var platforms
-=======
 let keyW,keyA,keyS,keyD
 let player
->>>>>>> Reward
 class GameScene extends Phaser.Scene{
 
     constructor(){
@@ -14,29 +13,22 @@ class GameScene extends Phaser.Scene{
     }
 
     preload(){
-<<<<<<< HEAD
             this.load.image('sky','images/sky.png');
-            this.load.image('glass','images/glass.png');
+            this.load.image('platform','images/platform.png')
+            this.load.image('Player','../../images/Gokuตัดเองจ้า.png',{frameWidth :384 ,frameHeight:216})
     }
     
     create(){
-            background = this.add.image(0,0,'sky').setOrigin(0).setScale(3)
-            platforms = this.physics.add.staticGroup();
-=======
-        this.load.image('Player','../../images/Gokuตัดเองจ้า.png',{frameWidth :384 ,frameHeight:216})
-
-    }
-    
-    create(){
-        player = this.physics.add.image(384,216,'Player').setSize(800,600).setScale(0.2).setCollideWorldBounds(true);
+        platforms = this.physics.add.image(0,0,'platform').setOrigin(0.5).setScale(1).setSize(1800,250).setCollideWorldBounds(true);
+        background = this.add.image(0,0,'sky').setOrigin(0).setScale(1)
+        
+  
+        player = this.physics.add.image(300,800,'Player').setSize(800,600).setScale(0.2).setCollideWorldBounds(true);
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        
->>>>>>> Reward
-
-            platforms.create(300, 750, 'glass').setScale(3).refreshBody();
+        this.physics.add.collider(player,platforms);
     }
 
     update(){
